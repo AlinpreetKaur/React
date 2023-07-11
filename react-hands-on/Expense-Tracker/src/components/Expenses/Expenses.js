bd5af768f5fc2f02cb8285506cff3187906afa33
AlinpreetKaur <36775500+AlinpreetKaur@users.noreply.github.com>
2023-07-11 13:05:37 +0530

    Assignment 2 (Parent to Child and Child to Parent Data Communication): Expense Filter Added

--- react-hands-on/Expense-Tracker/src/components/Expenses/Expenses.js
+++ react-hands-on/Expense-Tracker/src/components/Expenses/Expenses.js
@@ -1,10 +1,18 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import Card from "../UI/Cards/Cards";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 export default function Expenses(props) {
-
+  const [filterDate, setFilterDate] =  useState('');
+const onSelectHandler = (data) => {
+  console.log(data);
+setFilterDate(data);
+console.log(filterDate);
+}
     return (<Card className="expenses">
+      <ExpensesFilter selected={filterDate} onSelect={onSelectHandler}/>
         <ExpenseItem
         title={props.expenses[0].title}
         date={props.expenses[0].date}
